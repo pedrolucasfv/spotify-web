2b6b92f70e1cb65143be56999d660c426c075592
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2022-12-07 14:29:15 -0300

    .

---
+++ spotify-web-app/src/components/Player/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import Player from '.'
+
+export default {
+  title: 'Player',
+  component: Player
+} as Meta
+
+export const Default: Story = () => <Player />
